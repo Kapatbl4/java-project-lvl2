c4a5c93d41fce4a6ae28aa40f1f0b17ebc396ec8
kapatbl4 <vvi93@yandex.ru>
2022-04-27 15:25:38 +0300

    add Formatter.java

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -2,54 +2,78 @@ package hexlet.code;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.TreeSet;
 
 public class Differ {
 
     public static String generate(String pathToFile1, String pathToFile2) throws IOException {
-        HashMap<String, Object> resultOfFirstFile = Parser.parseFile(pathToFile1);
-        HashMap<String, Object> resultOfSecondFile = Parser.parseFile(pathToFile2);
+        Map<String, Object> resultOfFirstFile = Parser.parseFile(pathToFile1);
+        Map<String, Object> resultOfSecondFile = Parser.parseFile(pathToFile2);
 
-        LinkedHashMap<String, Object> result = new LinkedHashMap();
+        List<Map<String, Object>> interimDiff = genInterimDiff(resultOfFirstFile, resultOfSecondFile);
 
-        HashSet<String> keysSet = new HashSet<>();
-        keysSet.addAll(resultOfFirstFile.keySet());
-        keysSet.addAll(resultOfSecondFile.keySet());
-        List<String> keys = keysSet.stream().sorted().collect(Collectors.toList());
-
-        for (String s : keys) {
-            if (resultOfFirstFile.containsKey(s) && resultOfSecondFile.containsKey(s)) {
-                if (resultOfFirstFile.get(s).equals(resultOfSecondFile.get(s))) {
-                    result.put("   " + s, resultOfFirstFile.get(s));
-                } else {
-                    result.put(" - " + s, resultOfFirstFile.get(s));
-                    result.put(" + " + s, resultOfSecondFile.get(s));
-                }
-            } else if (resultOfFirstFile.containsKey(s) && !resultOfSecondFile.containsKey(s)) {
-                result.put(" - " + s, resultOfFirstFile.get(s));
-            } else if (!resultOfFirstFile.containsKey(s) && resultOfSecondFile.containsKey(s)) {
-                result.put(" + " + s, resultOfSecondFile.get(s));
-            }
-        }
-
-        return mapToString(result);
+        return Formatter.stylish(interimDiff);
     }
 
-    private static String mapToString(LinkedHashMap<String, Object> result) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("{\n");
-        for (Map.Entry<String, Object> pair : result.entrySet()) {
-            String key = pair.getKey();
-            String value = pair.getValue().toString();
-            sb.append(" ").append(key).append(": ").append(value).append("\n");
+    public static List<Map<String, Object>> genInterimDiff(Map<String, Object> map1, Map<String, Object> map2) {
+        List<Map<String, Object>> interimDiff = new ArrayList<>();
+        TreeSet<String> keysSet = new TreeSet<>(map1.keySet());
+        keysSet.addAll(map2.keySet());
+
+        for (String s : keysSet) {
+            Map<String, Object> element = new HashMap<>();
+            element.put("key", s);
+            element.put("oldValue", map1.getOrDefault(s, "No old value"));
+            element.put("newValue", map2.getOrDefault(s, "No new value"));
+
+            if (element.get("oldValue") == null && element.get("newValue") == null) {
+                element.put("status", "unchanged");
+            } else if (element.get("oldValue") == null && element.get("newValue") != null) {
+                if (element.get("newValue").equals("No new value")) {
+                    element.put("status", "deleted");
+                } else {
+                    element.put("status", "changed");
+                }
+            } else if (element.get("oldValue") != null && element.get("newValue") == null) {
+                if (element.get("oldValue").equals("No old value")) {
+                    element.put("status", "added");
+                } else {
+                    element.put("status", "changed");
+                }
+            } else if (element.get("oldValue") != null && element.get("newValue") != null) {
+                if (element.get("oldValue").equals("No old value") && !element.get("newValue").equals("No new value")) {
+                    element.put("status", "added");
+                } else if (!element.get("oldValue").equals("No old value")
+                        && element.get("newValue").equals("No new value")) {
+                    element.put("status", "deleted");
+                } else if (element.get("oldValue").equals(element.get("newValue"))) {
+                    element.put("status", "unchanged");
+                } else {
+                    element.put("status", "changed");
+                }
+            }
+//            } else if (element.get("oldValue") == null && element.get("newValue").equals("No new value")) {
+//                element.put("status", "deleted");
+//            } else if (element.get("oldValue") == null && !element.get("newValue").equals("No new value")) {
+//                element.put("status", "changed");
+//            } else if (element.get("oldValue").equals("No old value")
+//                        && !element.get("newValue").equals("No new value")) {
+//                element.put("status", "added");
+//            } else if (!element.get("oldValue").equals("No old value")
+//                        && element.get("newValue").equals("No new value")) {
+//                element.put("status", "deleted");
+//            } else if (element.get("oldValue").equals(element.get("newValue"))) {
+//                element.put("status", "unchanged");
+//            } else {
+//                element.put("status", "changed");
+//            }
+            interimDiff.add(element);
         }
-        sb.append("}");
-        return String.valueOf(sb);
+        return interimDiff;
     }
 
 }
--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -3,10 +3,13 @@ package hexlet.code;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     private static String expected;
+    private static String expected1;
 
     @BeforeAll
     static void setExpected() {
@@ -20,6 +23,35 @@ public class AppTest {
                 "}";
     }
 
+    @BeforeAll
+    static void setExpected1() {
+        expected1 = "{\n" +
+                "    chars1: [a, b, c]\n" +
+                "  - chars2: [d, e, f]\n" +
+                "  + chars2: false\n" +
+                "  - checked: false\n" +
+                "  + checked: true\n" +
+                "  - default: null\n" +
+                "  + default: [value1, value2]\n" +
+                "  - id: 45\n" +
+                "  + id: null\n" +
+                "  - key1: value1\n" +
+                "  + key2: value2\n" +
+                "    numbers1: [1, 2, 3, 4]\n" +
+                "  - numbers2: [2, 3, 4, 5]\n" +
+                "  + numbers2: [22, 33, 44, 55]\n" +
+                "  - numbers3: [3, 4, 5]\n" +
+                "  + numbers4: [4, 5, 6]\n" +
+                "  + obj1: {nestedKey=value, isNested=true}\n" +
+                "  - setting1: Some value\n" +
+                "  + setting1: Another value\n" +
+                "  - setting2: 200\n" +
+                "  + setting2: 300\n" +
+                "  - setting3: true\n" +
+                "  + setting3: none\n" +
+                "}";
+    }
+
     @Test
     public void testJSON() throws Exception {
         assertEquals(expected, Differ.generate("src/test/file1.json",
@@ -30,6 +62,18 @@ public class AppTest {
     public void testYML() throws Exception {
         assertEquals(expected, Differ.generate("src/test/file1.yml",
                 "src/test/file2.yml"));
+    }
+
+    @Test
+    public void testRecursiveJSON() throws IOException {
+        assertEquals(expected1, Differ.generate("src/test/file3.json",
+                "src/test/file4.json"));
+    }
+
+    @Test
+    public void testRecursiveYML() throws IOException {
+        assertEquals(expected1, Differ.generate("src/test/file3.yml",
+                "src/test/file4.yml"));
     }
 
 }
